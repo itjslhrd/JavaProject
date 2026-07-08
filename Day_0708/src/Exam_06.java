f33f65329dc816a4ec2a5a9244c6125650fd2a72
jongmin na <webs1027@gmail.com>
2026-07-08 17:46:49 +0900

    7월8월 최종

---
+++ Day_0708/src/Exam_06.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exam_06 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("1. 입력하시오");
+		//String str1 = scanner.next();//문자열1 문자열2
+		//String str2 = scanner.next();
+		
+		//System.out.print("2. 입력하시오");
+		String str3 = scanner.nextLine();//문자열1 문자열2 엔터치기까지
+		
+		//System.out.println("str1 : " + str1);
+		//System.out.println("str2 : " + str2);
+		System.out.println("str3 : " + str3);
+
+	}
+
+}
